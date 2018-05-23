decb4ecefe4a004874874f1de47def6cbd9d1c39
Rudolf Daniel <rudolf.daniel@outlook.com>
2018-05-23 21:36:12 +0200

    add some features

--- lib/checksum/checksum.rb
+++ lib/checksum/checksum.rb
@@ -1,4 +1,15 @@
 def checksum(article_text)
-  @new_article = article_text.gsub(/[^0-9a-z]/i, '').split('').each_slice(10).to_a
-  @new_article.map { |e| e.join("").capitalize! }
+  @first_state = article_text.gsub(/[^0-9A-Z]/i, '')
+  @second_state = @first_state.split('').each_slice(10).to_a
+  @second_state.map { |e| e.join("").capitalize! }
+#Here comes the uppercase of vovels
+=begin
+  @new_article.each do |i|
+    if i - 2 =~ /![aeiou]/ && i - 1 =~ /![aeiou]/
+      i.uppercase!
+    end
+  end
+=end
+#Here comes the sum of the specified checksums
+  return @first_state.length.to_s + "-" + @second_state.length.to_s
 end

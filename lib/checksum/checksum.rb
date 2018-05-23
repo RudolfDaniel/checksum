27da403ad3f29f3a9b3d7c0e150068c9f963706b
Rudolf Daniel <rudolf.daniel@outlook.com>
2018-05-23 20:45:59 +0200

    uppercase strings

--- lib/checksum/checksum.rb
+++ lib/checksum/checksum.rb
@@ -1,3 +1,4 @@
 def checksum(article_text)
-  article_text.gsub(/[^0-9a-z]/i, '').split('').each_slice(10).to_a
+  @new_article = article_text.gsub(/[^0-9a-z]/i, '').split('').each_slice(10).to_a
+  @new_article.map { |e| e.join("").capitalize! }
 end
